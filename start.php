859eeb80230ca72eb1a585be0c9e9446a19f2049
29da164 <66115554+29da164@users.noreply.github.com>
2021-09-18 13:22:31 +0900

    Add files via upload

---
+++ start.php
@@ -0,0 +1,9 @@
+<html>
+  <body>
+    <form method="POST" action="./ff.php">
+      Year:  <input type="text" size="5" name="year"/><br />
+      Month: <input type="text" size="5" name="month"/><br />
+      <input type="submit" value="send" />
+    </form>
+  </body>
+</html> 
